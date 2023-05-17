424e6064117226ba292a3b71f8c4f76bfbbbf2e4
lincoln gibson <lincolngibson7@gmail.com>
2023-05-17 09:19:47 -0300

    Add modules to separate concerns and reduce size.

---
+++ src/modules/getScore.js
@@ -0,0 +1,13 @@
+import url from './api.js';
+
+const getScore = async () => {
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		return data.result;
+	} catch (error) {
+		return error.message || 'Something went wrong';
+	}
+};
+
+export default getScore;
---
+++ src/modules/renderScores.js
@@ -0,0 +1,27 @@
+const nameInput = document.querySelector('#name-input');
+const scoreInput = document.querySelector('#score-input');
+
+const renderScores = (scoreItem) => {
+	const scoresTable = document.querySelector('table');
+	const tableBody = document.querySelector('tbody');
+
+	tableBody.innerHTML = '';
+	scoreItem.forEach((score) => {
+		const tableRow = document.createElement('tr');
+
+		const nameItem = document.createElement('td');
+		nameItem.classList.add('player-name');
+		nameItem.innerText = `${score.user}`;
+		tableRow.appendChild(nameItem);
+
+		const scoreItem = document.createElement('td');
+		scoreItem.classList.add('player-score');
+		scoreItem.innerText = `${score.score}`;
+		tableRow.appendChild(scoreItem);
+
+		tableBody.appendChild(tableRow);
+		scoresTable.appendChild(tableBody);
+	});
+};
+
+export default renderScores;

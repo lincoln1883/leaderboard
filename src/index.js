9f03dca63fe8d43e1cf3d39766a2b728d3aa60ed
lincoln gibson <lincolngibson7@gmail.com>
2023-05-17 09:28:41 -0300

    Fix linter errors.

--- src/index.js
+++ src/index.js
@@ -9,23 +9,23 @@ const scoreInput = document.querySelector('#score-input');
 const refreshBtn = document.querySelector('#refresh-btn');
 
 const handleSubmit = async (e) => {
-	e.preventDefault();
-	const user = nameInput.value;
-	const score = scoreInput.value;
-	const results = await addScore(user, score);
-	if (results && results.result === 'Leaderboard score created correctly.') {
-		const scoreItem = await getScore();
-		renderScores(scoreItem);
-	}
-	scoreInput.value = '';
-	nameInput.value = '';
+  e.preventDefault();
+  const user = nameInput.value;
+  const score = scoreInput.value;
+  const results = await addScore(user, score);
+  if (results && results.result === 'Leaderboard score created correctly.') {
+    const scoreItem = await getScore();
+    renderScores(scoreItem);
+  }
+  scoreInput.value = '';
+  nameInput.value = '';
 };
 
 const handleRefresh = async () => {
-	const tableBody = document.querySelector('tbody');
-	tableBody.innerHTML = '';
-	const scores = await getScore();
-	renderScores(scores);
+  const tableBody = document.querySelector('tbody');
+  tableBody.innerHTML = '';
+  const scores = await getScore();
+  renderScores(scores);
 };
 
 form.addEventListener('submit', handleSubmit);
--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,23 +1,23 @@
 import url from './api.js';
 
 const addScore = async (user, score) => {
-	const scoreItem = {
-		user,
-		score,
-	};
-	try {
-		const response = await fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(scoreItem),
-			headers: {
-				'Content-type': 'application/json',
-			},
-		});
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		return error.message;
-	}
+  const scoreItem = {
+    user,
+    score,
+  };
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(scoreItem),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 export default addScore;

acdb9591a3c15bfdc332d4947327f4934724c254
lincoln gibson <lincolngibson7@gmail.com>
2023-05-17 13:43:30 -0300

    Fix linter errors.

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,24 +1,24 @@
 const renderScores = (scoreItem) => {
-	const scoresTable = document.querySelector('table');
-	const tableBody = document.querySelector('tbody');
+  const scoresTable = document.querySelector('table');
+  const tableBody = document.querySelector('tbody');
 
-	tableBody.innerHTML = '';
-	scoreItem.forEach((score) => {
-		const tableRow = document.createElement('tr');
+  tableBody.innerHTML = '';
+  scoreItem.forEach((score) => {
+    const tableRow = document.createElement('tr');
 
-		const nameItem = document.createElement('td');
-		nameItem.classList.add('player-name');
-		nameItem.innerText = `${score.user}:`;
-		tableRow.appendChild(nameItem);
+    const nameItem = document.createElement('td');
+    nameItem.classList.add('player-name');
+    nameItem.innerText = `${score.user}:`;
+    tableRow.appendChild(nameItem);
 
-		const scoreItem = document.createElement('td');
-		scoreItem.classList.add('player-score');
-		scoreItem.innerText = `${score.score}`;
-		tableRow.appendChild(scoreItem);
+    const scoreItem = document.createElement('td');
+    scoreItem.classList.add('player-score');
+    scoreItem.innerText = `${score.score}`;
+    tableRow.appendChild(scoreItem);
 
-		tableBody.appendChild(tableRow);
-		scoresTable.appendChild(tableBody);
-	});
+    tableBody.appendChild(tableRow);
+    scoresTable.appendChild(tableBody);
+  });
 };
 
 export default renderScores;

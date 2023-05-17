bd2fd2684e0db423bafd60d8b81eb40561bb42de
lincoln gibson <lincolngibson7@gmail.com>
2023-05-17 13:36:21 -0300

    Add function for displaying the message in the ui.

--- src/index.js
+++ src/index.js
@@ -8,24 +8,43 @@ const nameInput = document.querySelector('#name-input');
 const scoreInput = document.querySelector('#score-input');
 const refreshBtn = document.querySelector('#refresh-btn');
 
+const displayMessage = (message, isSuccess) => {
+	const messageBox = document.querySelector('.message');
+	messageBox.innerHTML = message;
+	if (isSuccess) {
+		messageBox.classList.add('success');
+		messageBox.classList.remove('error');
+	} else {
+		messageBox.classList.add('error');
+		messageBox.classList.remove('success');
+	}
+
+	setTimeout(() => {
+		messageBox.innerHTML = '';
+	}, 3000);
+};
+
 const handleSubmit = async (e) => {
-  e.preventDefault();
-  const user = nameInput.value;
-  const score = scoreInput.value;
-  const results = await addScore(user, score);
-  if (results && results.result === 'Leaderboard score created correctly.') {
-    const scoreItem = await getScore();
-    renderScores(scoreItem);
-  }
-  scoreInput.value = '';
-  nameInput.value = '';
+	e.preventDefault();
+	const user = nameInput.value;
+	const score = scoreInput.value;
+	const results = await addScore(user, score);
+	if (results && results.result === 'Leaderboard score created correctly.') {
+		const scoreItem = await getScore();
+		renderScores(scoreItem);
+		displayMessage(results.result, true);
+	} else {
+		displayMessage(results.error, false);
+	}
+	scoreInput.value = '';
+	nameInput.value = '';
 };
 
 const handleRefresh = async () => {
-  const tableBody = document.querySelector('tbody');
-  tableBody.innerHTML = '';
-  const scores = await getScore();
-  renderScores(scores);
+	const tableBody = document.querySelector('tbody');
+	tableBody.innerHTML = '';
+	const scores = await getScore();
+	renderScores(scores);
 };
 
 form.addEventListener('submit', handleSubmit);
